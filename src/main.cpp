1770ecb53fe15bd99a885ebdd4b39109a3cff040
Bonfell <porkhunov99@inbox.ru>
2026-05-30 23:16:14 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
 #include "../include/bst.h"
-
 #include <iostream>
 
 void makeTree(BST<std::string>& tree, const char* filename);
@@ -10,6 +9,9 @@ int main() {
   BST<std::string> tree;
   makeTree(tree, "src/war_peace.txt");
   if (!tree.isEmpty()) {
+    std::cout << "Frequency of 'the' : " << tree.getFrequency("the") << std::endl;
+    std::cout << "Frequency of 'and' : " << tree.getFrequency("and") << std::endl;
+    std::cout << "Frequency of 'to'  : " << tree.getFrequency("to") << std::endl;
     printFreq(tree);
   }
   return 0;
