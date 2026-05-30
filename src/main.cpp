499f2a29233addfc3dee51d6a48c11d1a10b4712
Bonfell <porkhunov99@inbox.ru>
2026-05-30 21:55:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-#include <iostream>
+#include "../include/bst.h"
+
 #include <chrono>
+#include <iostream>
+#include <string>
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
-    
+
     std::cout << "Starting frequency analysis..." << std::endl;
-    
+
     makeTree(tree, "src/war_peace.txt");
-    
+
     if (!tree.isEmpty()) {
         printFreq(tree);
     } else {
         std::cout << "Failed to build tree. File may not exist or is empty." << std::endl;
     }
-    
+
     return 0;
 }
