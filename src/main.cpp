725acefd6d7b89aed2c1aeecef54df9275ee1284
Bonfell <porkhunov99@inbox.ru>
2026-05-30 22:47:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,17 @@ void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> wordTree;
-    
+
     makeTree(wordTree, "src/war_peace.txt");
 
     std::cout << "Глубина дерева: " << wordTree.depth() << std::endl;
     std::cout << "Поиск слова 'war': " << (wordTree.search("war") ? "Найдено" : "Не найдено") << std::endl;
-    
+
     std::cout << "\nЧастотный анализ (топ слов):" << std::endl;
     printFreq(wordTree);
 
     return 0;
 }
+
 
 
